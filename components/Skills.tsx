ae2c0c876fcbf047cb6cbf5133b48b30c8c0544c
bang07112004 <phong0909191145@gmail.com>
2022-11-23 00:07:07 +0700

    commit

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -22,7 +22,7 @@ function Skills({skills}: Props) {
             Skills
         </h3>
         <h3 className='absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm'>
-            Hover over a skill for current proficiency
+            Hover over a skill for current proficiency (change)
         </h3>
         <div className='grid grid-cols-4 gap-5'>
             {skills?.slice(0,skills.length / 2).map(skill => (
